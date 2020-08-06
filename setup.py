ca7aa55ff5d231b729e9fa11d444dc96cffd07d7
binghong <binghong@gatech.edu>
2020-08-06 16:10:24 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='multiobj-rationale',
+    version='https://github.com/wengong-jin/multiobj-rationale',
+    packages=['scripts', 'fuseprop'],
+    url='',
+    license='MIT',
+    author='Wengong Jin',
+    author_email='',
+    description='Multi-Objective Molecule Generation using Interpretable Substructures'
+)
